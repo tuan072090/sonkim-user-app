3b2d37dbc663f615d0782f846f3868f1a10b0773
Tien Tai Nguyen <nguyentientai.3325@gmail.com>
2021-10-13 17:53:35 +0700

    Account Screen

--- src/screens/account/components/AccountItem.tsx
+++ src/screens/account/components/AccountItem.tsx
@@ -1,23 +1,32 @@
-import { Button } from 'native-base'
+import { Box, Button, Center, HStack } from 'native-base'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { AccountItemType } from './AccountType'
 
 const AccountItem:React.FC<AccountItemType> = ({item,...props}) => {
     return (
-        <Button colorScheme="white" rounded="lg" startIcon={item.startIcon} endIcon={item.endIcon} _text={{
-            fontStyle:'normal',
-            fontWeight:'semibold',
-            fontSize:'md',
-            lineHeight:'md',
-            letterSpacing:'lg',
-            color:'#626262'
-        }}
-            shadow={1}
-        >{item.title}</Button>
+            <Box bgColor="white" borderRadius={12} height="12" mb="3" 
+                shadow={4}
+            >
+                <HStack alignItems="center" justifyContent="space-between" py={2} px={5}   >
+                    <HStack space={2} alignItems="center" justifyContent="space-around">
+                        {item.startIcon}
+                        <Box _text={{
+                                fontStyle:'normal',
+                                fontWeight:'semibold',
+                                fontSize:'md',
+                                lineHeight:'md',
+                                letterSpacing:'lg',
+                                color:'#626262'
+                        }}>
+                {item.title}</Box>
+                    </HStack>
+                    {item.endIcon}
+                </HStack>
+                
+            </Box>
     )
 }
 
 export default AccountItem
 
-const styles = StyleSheet.create({})
--- src/screens/account/index.tsx
+++ src/screens/account/index.tsx
@@ -1,13 +1,60 @@
+import { Box, Button, Center, Switch } from "native-base";
 import React from "react";
 import {StyleSheet, Text, View} from "react-native";
+import { ChevronRightIcon, FriendIcon, LocationIcon, NotificationIcon, NotificationOutlineIcon, TranslateIcon } from "../../components";
 import AccountHeader from "./components/AccountHeader";
+import AccountItem from "./components/AccountItem";
+
+const sampleData=[
+    {
+        startIcon:<LocationIcon size={8}></LocationIcon>,
+        title:'Lịch sử đổi điểm',
+        endIcon:<ChevronRightIcon size={8}></ChevronRightIcon>
+    },
+    {
+        startIcon:<FriendIcon size={8}></FriendIcon>,
+        title:'Friend refferal',
+        endIcon:<ChevronRightIcon size={8}></ChevronRightIcon>
+    },
+    {
+        startIcon:<TranslateIcon size={8}></TranslateIcon>,
+        title:'Ngôn ngữ',
+        endIcon:<ChevronRightIcon size={8}></ChevronRightIcon>
+    },
+    {
+        startIcon:<NotificationOutlineIcon size={8}></NotificationOutlineIcon>,
+        title:'Lịch sử đổi điểm',
+        endIcon:<Switch size={8}></Switch>,
+    },
+]
 
 const AccountScreen = () => {
 
     return (
         <View style={styles.wrap}>
             <AccountHeader></AccountHeader>
-            <Text>Account Screen</Text>
+            <Box p="4">
+                {sampleData.map((item,index)=>(
+                    <AccountItem key={index} item={item}></AccountItem>
+                ))}
+                <Center bgColor="#08698133"
+                mt={2}
+                height={12}
+                borderRadius={12}
+                _text={{
+                    fontStyle:'normal',
+                    fontWeight:'bold',
+                    fontSize:'md',
+                    lineHeight:'lg',
+                    letterSpacing:'lg',
+                    color:'#086981'
+                }}
+                >
+                    Đăng xuất
+                </Center>
+                
+            </Box>
+            
         </View>
     )
 }
