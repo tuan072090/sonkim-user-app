0681775184a1ecc8b51fc3ebf52c3902d0468c55
Tien Tai Nguyen <nguyentientai.3325@gmail.com>
2021-10-18 21:28:24 +0700

    voucherItem component

--- src/screens/BU-detail/components/VoucherItem.tsx
+++ src/screens/BU-detail/components/VoucherItem.tsx
@@ -1,17 +1,26 @@
 import { Box, HStack, VStack,Text } from 'native-base'
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { CheckStoreIcon, HistoryIcon } from '../../../components'
 type VoucherItemType={
 
 }
 
 const VoucherItem:React.FC<VoucherItemType> = () => {
     return (
-        <VStack>
-            <HStack alignItems="center">
-                <Box bgColor="primary" rounded="lg" width="20" height="20"></Box>
-                <VStack>
-                    <Text fontWeight="semibold" fontStyle="normal" fontSize="md" lineHeight="md" color="darkText">Voucher giảm 20% cho dịch vụ massage vào tháng 10</Text>
+        <VStack bgColor="white" borderRadius={6} shadow={4}>
+            <HStack mx={3} my={2}>
+                <Box bgColor="primary.500" borderRadius={6} width="20" height="20" ></Box>
+                <VStack ml={3}>
+                    <Text fontWeight="semibold" fontStyle="normal" fontSize="md" lineHeight="md" color="darkText" width="64" bgColor="primary.900" >Voucher giảm 20% cho dịch vụ massage vào tháng 10</Text>
+                    <HStack>
+                        <CheckStoreIcon size={4}></CheckStoreIcon>
+                        <Text fontWeight="normal" fontStyle="normal" fontSize="xs" lineHeight="2xs" letterSpacing="lg" color="#626262" textDecoration="underline">Áp dụng cho 5 chi nhánh</Text>
+                    </HStack>
+                    <HStack>
+
+                        <HistoryIcon size={4}></HistoryIcon>
+                        <Text fontWeight="normal" fontStyle="normal" fontSize="xs" lineHeight="2xs" letterSpacing="lg" color="#626262" textDecoration="underline">Hạn sử dụng: 12/21/2021</Text>
+                    </HStack>
                 </VStack>
             </HStack>
         </VStack>
@@ -20,4 +29,3 @@ const VoucherItem:React.FC<VoucherItemType> = () => {
 
 export default VoucherItem
 
-const styles = StyleSheet.create({})
