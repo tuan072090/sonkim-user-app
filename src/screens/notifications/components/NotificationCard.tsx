cb62505f7731c924fc1d9f2119aa14998d98aed9
Tien Tai Nguyen <nguyentientai.3325@gmail.com>
2021-10-20 14:44:46 +0700

    update data, done Notification

--- src/screens/notifications/components/NotificationCard.tsx
+++ src/screens/notifications/components/NotificationCard.tsx
@@ -1,25 +1,21 @@
 import { Box, HStack, Text, VStack } from 'native-base'
 import React from 'react'
 import { ImageStatic } from '../../../components'
-import { StaticImages } from '../../../share'
-type NotificationCardType={
+import { NotificationCardType } from './NotificationType'
 
-}
-
-const NotificationCard:React.FC<NotificationCardType> = () => {
+const NotificationCard:React.FC<NotificationCardType> = ({item,...props}) => {
     return (
         <HStack alignItems="flex-start" mt={4} mx={4} borderRadius={12} bgColor="white">
-            <ImageStatic uri={StaticImages.lazada} width={8} height={8} my={4} ml={3}></ImageStatic>
+            <ImageStatic uri={item.logo} width={8} height={8} my={4} ml={3}></ImageStatic>
             <VStack justifyContent="center" my={4} mx={3}>
                 <HStack justifyContent="space-between">
-                    <Text fontWeight="semibold" fontStyle="normal" fontSize="md" lineHeight="md" color="#1B1B1B" letterSpacing="lg" mb={3}>Đăng ký thành công thẻ Lazada</Text>
-                    <Box width={2} height={2} borderRadius={48} bgColor="#951540" mr={4} mt={2}></Box>
+                    <Text fontWeight="semibold" fontStyle="normal" fontSize="md" lineHeight="md" color="#1B1B1B" letterSpacing="lg" mb={3} width={64}>{item.title}</Text>
+                    {item.unread && <Box width={2} height={2} borderRadius={48} bgColor="#951540" mr={4} mt={2}></Box>}
                 </HStack>
-                <Text fontStyle="normal" fontWeight="normal" fontSize="xs" lineHeight="2xs" letterSpacing="lg" color="#C8C8C8" mb={1}>12/12/2021 - 12:00</Text>
-                <Text fontStyle="normal" fontWeight="normal" lineHeight="xs" color="#626262" letterSpacing="md" width={80}>Chúc mừng bạn đã đăng ký thành công thẻ thành viên của Lazada</Text>
+                <Text fontStyle="normal" fontWeight="normal" fontSize="xs" lineHeight="2xs" letterSpacing="lg" color="#C8C8C8" mb={1}>{item.due}</Text>
+                <Text fontStyle="normal" fontWeight="normal" lineHeight="xs" color="#626262" letterSpacing="md" width={80}>{item.description}</Text>
             </VStack>
         </HStack> 
-
     )
 }
 
--- src/screens/notifications/index.tsx
+++ src/screens/notifications/index.tsx
@@ -5,6 +5,37 @@ import {Translate} from "../../share";
 import LanguageProvider from "../../share/context/Language";
 import { CheckAllIcon } from "../../components";
 import NotificationCard from "./components/NotificationCard";
+import { StaticImages } from '../../share'
+
+
+const notificationSampleData=[
+    {
+        logo: StaticImages.lazada,
+        title:'Đăng kí thành công thẻ Lazada',
+        due:'12/12/2021 - 12:00',
+        description:'Chúc mừng bạn đã đăng kí thành công thẻ thành viên của Lazada',
+        unread:true,
+    },
+    {
+        logo: StaticImages.cgv,
+        title:'Bạn đã trở thành thành viên của CGV Cinema',
+        due:'12/12/2021 - 12:00',
+        description:'Chúc mừng bạn đã đăng kí thành công thẻ thành viên của Lazada',
+        unread:true
+    },
+    {
+        logo: StaticImages.gs25,
+        title:'Bạn đã được tặng 1 voucher',
+        due:'12/12/2021 - 12:00',
+        description:'Bạn đã được tặng 1 voucher 50% khi mua hóa đơn trên 100k'
+    },
+    {
+        logo: StaticImages.cgv,
+        title:'Bạn đã nhận được một sản phẩm tri ân khách hàng',
+        due:'12/12/2021 - 12:00',
+        description:'Chúc mừng bạn đã trở thành thành viên vàng của Jockey. Bạn sẽ nhận được một...'
+    },
+]
 
 const NotificationsScreen = () => {
     const {language} = useContext(LanguageProvider.context)
@@ -13,8 +44,11 @@ const NotificationsScreen = () => {
         <Box flex={1} >
             <ScreenHeader hasBackButton={true} title={Translate[language].notifications} bgColor="primary.500" rightIcon={<CheckAllIcon size={6} />}/>
             <ScrollView>
-                <NotificationCard></NotificationCard>
-                <NotificationCard></NotificationCard>
+                {
+                    notificationSampleData.map((item,index)=>(
+                        <NotificationCard item={item} key={index}></NotificationCard>
+                    ))
+                }
             </ScrollView>
         </Box>
     )
